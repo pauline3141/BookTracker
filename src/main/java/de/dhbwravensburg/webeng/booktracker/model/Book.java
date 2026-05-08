e4eb237812d950d0929d47ba8e70996a6e010fa4
pauline3141 <paulinehuber6@gmail.com>
2026-05-08 16:24:50 +0200

    Add Book model with Lombok

---
+++ src/main/java/de/dhbwravensburg/webeng/booktracker/model/Book.java
@@ -0,0 +1,18 @@
+package de.dhbwravensburg.webeng.booktracker.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private String coverUrl;
+    private int publishYear;
+}
