4815dae95d928ffc8adc0056d081f2b7f5f8a345
pauline3141 <paulinehuber6@gmail.com>
2026-05-14 18:57:39 +0200

    Convert Shelf to JPA entity with @Entity and @Id annotations

--- src/main/java/de/dhbwravensburg/webeng/booktracker/model/Shelf.java
+++ src/main/java/de/dhbwravensburg/webeng/booktracker/model/Shelf.java
@@ -1,17 +1,25 @@
 package de.dhbwravensburg.webeng.booktracker.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Shelf {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String description;
     private LocalDateTime createdAt;
