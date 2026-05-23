f09ac38b03226b906ef87fac9aac1b7700a8bab7
pauline3141 <paulinehuber6@gmail.com>
2026-05-23 18:01:17 +0200

    Add ShelfEntryRequest and ShelfEntryResponse DTOs

---
+++ src/main/java/de/dhbwravensburg/webeng/booktracker/dto/ShelfEntryResponse.java
@@ -0,0 +1,13 @@
+package de.dhbwravensburg.webeng.booktracker.dto;
+
+import de.dhbwravensburg.webeng.booktracker.model.ReadingStatus;
+
+import java.time.LocalDateTime;
+
+public record ShelfEntryResponse(
+        Long id,
+        Long shelfId,
+        BookResponse book,
+        ReadingStatus status,
+        LocalDateTime addedAt
+) {}
