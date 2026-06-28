90d9785e20a5b52ceb95eb102fa3b365bcf345e6
pauline3141 <paulinehuber6@gmail.com>
2026-06-28 15:18:25 +0200

    Add JWT security scheme to Swagger UI

--- src/main/java/de/dhbwravensburg/webeng/booktracker/config/OpenApiConfig.java
+++ src/main/java/de/dhbwravensburg/webeng/booktracker/config/OpenApiConfig.java
@@ -1,4 +1,35 @@
 package de.dhbwravensburg.webeng.booktracker.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI bookTrackerOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("BookTracker API")
+                        .version("0.1.0")
+                        .description("Search for books, add them to shelves, and track reading progress")
+                        .contact(new Contact()
+                                .name("DHBW Web-Engineering")
+                                .email("noreply@dhbw.de")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                                .name(SECURITY_SCHEME_NAME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
 }
